fc24ad43536db6c8ee7832a574ceee33962fa901
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-21 20:11:11 +0300

    update routes

--- project/APIs/urls.py
+++ project/APIs/urls.py
@@ -4,6 +4,7 @@ from .views import(
     update_user_view,
     page404_view,
     register_view,
+    update_password_view,
 )
 from rest_framework_simplejwt.views import (
     TokenRefreshView, 
@@ -13,7 +14,8 @@ urlpatterns = [
     path('token/refresh', TokenRefreshView.as_view(), name='refresh'),
     path('auth/register', register_view, name='register-user'),
     path('auth/login', login_view, name='login-user'),
-    path('user/update', update_user_view, name='update-user'),
+    path('user/update/profile', update_user_view, name='update-user'),
+    path('user/update/password', update_password_view, name='update-password'),
 ]
 
 urlpatterns += [
