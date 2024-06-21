836f85df03734c7e725f49e5d085292d9463fbb3
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-21 20:10:43 +0300

    update views

--- project/APIs/views.py
+++ project/APIs/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import update_session_auth_hash
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import (
+    MyTokenObtainPairSerializer,
     CustomerSerializer, 
-    MyTokenObtainPairSerializer
+    UpdatePasswordSerializer,
+    UpdateProfileSerializer,
 )
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -41,10 +44,23 @@ class LoginUserAPIView(TokenObtainPairView):
         
 
 class UpdateUserAPIView(APIView):
-    permission_classes = (IsAuthenticated,) 
-    
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, *args, **kwargs):
-        return Response({"data": "OK"}, status=status.HTTP_200_OK)
+        serializer = UpdateProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            user.first_name = serializer.validated_data.get('first_name')
+            user.last_name = serializer.validated_data.get('last_name')
+            user.phone_number = serializer.validated_data.get('phone_number')
+            user.save()
+            # return response OK
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        # Invalidated data
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+        # return Response({"data": f"{user.username} used their {token.payload.get('token_type')} token"}, status=status.HTTP_200_OK)
 
 
 class PageNotFoundAPIView(APIView):
@@ -69,8 +85,26 @@ class RegisterUserAPIView(APIView):
             return Response(user, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class UpdateUserPasswordAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UpdatePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user 
+            if user.check_password(serializer.validated_data.get('old_password')):
+                user.set_password(serializer.validated_data.get('new_password'))
+                user.save()
+                update_session_auth_hash(request, user)
+                return Response({"data":"Password updated successfully"}, status=status.HTTP_200_OK)
+            return Response({"data": "Incorrect old password"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 register_view = RegisterUserAPIView.as_view()
 login_view = LoginUserAPIView.as_view()
 update_user_view = UpdateUserAPIView.as_view()
+update_password_view = UpdateUserPasswordAPIView.as_view()
 page404_view = PageNotFoundAPIView.as_view()
