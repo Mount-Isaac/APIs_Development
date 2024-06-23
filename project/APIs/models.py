7de2c6fca132ca460b7099c0aaddffd2b45e600b
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-23 09:37:27 +0300

    create customer model

--- project/APIs/models.py
+++ project/APIs/models.py
@@ -1,67 +1,40 @@
-# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-# from django.contrib.auth.base_user import BaseUserManager
-# from django.db import models
-
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, email, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError("The Email field must be set")
-        
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save()
-
-#         return user
-    
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         return self.create_user(email, password, **extra_fields)
-    
-
-# class CustomUser(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(unique=True)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     is_active = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-
-#     objects = CustomUserManager()
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name']
-
-#     def __str__(self):
-#         return self.email
-
-
-# class Client(models.Model):
-#     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='client')
-#     phone_number = models.CharField(max_length=20)
-#     address = models.TextField()
-
-#     def __str__(self):
-#         return f'{self.user.first_name} {self.user.last_name}'
-
-
 from django.db import models
 from django.contrib.auth.models import User
 
+
+# whenever a user deletes their account and they 
+# have Blogs associated with them, transfer the 
+# ownership to the admin
+# admin_user = User.objects.get(username='admin')
+
 class Customer(models.Model):
+    '''
+        EXPECTED JSON OBJECT FORMAT
+        {
+            "email": "isaackyalo@mail.co.ke",
+            "first_name": "isaac",
+            "last_name": "kyalo",
+            "phone_number": "255982018924",
+            "password": "#8sj92980uojksf"
+        }
+    '''
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
     email = models.EmailField(max_length=80, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=50)
+    # add user cloudinary image upload
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-    
-# {
-#   "email": "isaackyalo@mail.co.ke",
-#   "first_name": "isaac",
-#   "last_name": "kyalo",
-#   "phone_number": "255982018924",
-#   "password": "#8sj92980uojksf"
-# }
+
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=30)
+    content = models.TextField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # add cloudinary image upload
+
+    def __str__(self):
+        return f"{self.title.capitalize()}"
