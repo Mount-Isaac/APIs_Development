ccca5e64b62c460f4bc11e5ddaf5be4c30914a42
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-24 12:32:26 +0300

    add image field in Customer, Blog models

--- project/APIs/models.py
+++ project/APIs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 # whenever a user deletes their account and they 
@@ -23,7 +24,12 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=50)
-    # add user cloudinary image upload
+    image = CloudinaryField(
+        'image', 
+        null=True, 
+        blank=True, 
+        default='https://res.cloudinary.com/dkzk3c8rt/image/upload/v1719169562/APIs_customers/ja0cm5vzpjjzkzfhkpcs.png'
+    )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -31,9 +37,10 @@ class Customer(models.Model):
 
 class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
     content = models.TextField(blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     # add cloudinary image upload
 
     def __str__(self):
