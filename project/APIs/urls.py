bbfde0fa9cd84a374c4a31c6c043c97cab85393a
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-24 12:33:09 +0300

    add blog CRUD routes

--- project/APIs/urls.py
+++ project/APIs/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, re_path
 from .views import(
     login_view,
-    update_user_view,
     page404_view,
     register_view,
+    update_user_view,
     update_password_view,
+    update_profile_picture,
+    create_blog_view,
+    update_blog_view,
+    all_blogs_view,
+    delete_blog_view,
 )
 from rest_framework_simplejwt.views import (
     TokenRefreshView, 
@@ -16,6 +21,12 @@ urlpatterns = [
     path('auth/login', login_view, name='login-user'),
     path('user/update/profile', update_user_view, name='update-user'),
     path('user/update/password', update_password_view, name='update-password'),
+    path('user/update/image', update_profile_picture, name='update-image'),
+    path('user/create/blog', create_blog_view, name='create-blog'),
+    path('user/update/blog/<str:id>', update_blog_view, name='update-blog'),
+    path('all/blogs', all_blogs_view, name='all-blogs'),
+    path('user/delete/blog/<str:id>', delete_blog_view, name='delete-blog'),
+
 ]
 
 urlpatterns += [
