cb2d35e8b83fa1ae6bce42b0a4f9b13e891fa197
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-24 12:32:03 +0300

    config ready() for signals

--- project/APIs/apps.py
+++ project/APIs/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'APIs'
+    
+    def ready(self):
+        import APIs.signals
