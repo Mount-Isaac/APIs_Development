1f29086695c7a50d81d8eb3f8038168a9592ac3d
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-24 12:33:42 +0300

    CRUD operations views defined using APIView class

--- project/APIs/views.py
+++ project/APIs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import update_session_auth_hash
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
@@ -6,11 +6,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .models import Blog, Customer
 from .serializers import (
     MyTokenObtainPairSerializer,
     CustomerSerializer, 
     UpdatePasswordSerializer,
     UpdateProfileSerializer,
+    UpdateProfilePictureSerializer,
+    CreateBlogPostSerializer,
+    UpdateBlogPostSerializer,
 )
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -101,10 +106,94 @@ class UpdateUserPasswordAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class UpdateProfilePictureAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+        try:
+            user = Customer.objects.get(user=User.objects.get(id=4))
+
+            # data = {
+            #     'image':'https://res.cloudinary.com/dkzk3c8rt/image/upload/v1719173491/tlpahhl7yu2ur0zqpgjt.png'
+            # }
+            
+            serializer = UpdateProfilePictureSerializer(instance=user, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                # return only the image url
+                data = serializer.data
+                data['image'] = f"https:{''.join(data.get('image').split(':')[1:])}"
+                return Response(data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"Error": str(e).strip()}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateBlogPostAPIView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        # valid user -> request.user
+        user = get_object_or_404(User, id=4)
+        serializer = CreateBlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class UpdateBlogPostAPIView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def post(self, request, id, *args, **kwargs):
+        # replace with request.user.id
+        user = get_object_or_404(User,id=4)
+
+        # blog id is a parameterized variable
+        blog_obj = get_object_or_404(Blog, id=id)
+
+        # validate blog author 
+        if blog_obj.author != user:
+            return Response({"Error": "Only the blog author can update their post."}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = UpdateBlogPostSerializer(blog_obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class AllBlogPostsAPIView(APIView):
+    def get(self, *args, **kwargs):
+        blogs = Blog.objects.all()
+        serializer = CreateBlogPostSerializer(blogs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserBlogPosts(APIView):
+    pass
+
+class DeleteBlogPostAPIView(APIView):
+    def delete(self, request, id, *args,**kwargs):
+        # replace with request.user.id
+        user = get_object_or_404(User, id=4)
+
+        # id is passed as a parameterized query
+        blog_obj = get_object_or_404(Blog, id=id)
+
+        # validate blog author
+        if blog_obj.author != user:
+            return Response({"Error": "Only the blog author can delete their post."}, status=status.HTTP_403_FORBIDDEN)
+        
+        blog_obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 register_view = RegisterUserAPIView.as_view()
 login_view = LoginUserAPIView.as_view()
 update_user_view = UpdateUserAPIView.as_view()
 update_password_view = UpdateUserPasswordAPIView.as_view()
+update_profile_picture = UpdateProfilePictureAPIView.as_view()
+create_blog_view = CreateBlogPostAPIView.as_view()
+update_blog_view = UpdateBlogPostAPIView.as_view()
+delete_blog_view = DeleteBlogPostAPIView.as_view()
+all_blogs_view = AllBlogPostsAPIView.as_view()
 page404_view = PageNotFoundAPIView.as_view()
