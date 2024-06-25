68152a6dcc3665ab6b8543e5536e388228aff279
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-25 11:27:58 +0300

    homepage updated with bootstrap

--- frontend/src/components/pages/universal/Homepage.js
+++ frontend/src/components/pages/universal/Homepage.js
@@ -1,6 +1,6 @@
 export default function Homepage() {
     return ( 
-        <div className="container p-5">
+        <div className="container p-3">
             <div className="text-center">
                 <h1 className="text-primary">Welcome to APIs Development</h1>
                 <pre style={{fontWeight:600}}>Using Django, REST API and ReactJS</pre>
