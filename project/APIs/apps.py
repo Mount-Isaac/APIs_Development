afcbfdc1bfeb852c9728ddd778ccb6fef54dc833
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-24 22:57:16 -0700

    Delete project directory

--- project/APIs/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'APIs'
-    
-    def ready(self):
-        import APIs.signals
