518558ba03cccbdf2a697f12585161496b603b8b
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-25 11:27:25 +0300

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,12 @@ import { AuthProvider } from './components/utils/AuthProvider'
 
 // components
 import Homepage from './components/pages/universal/Homepage'
+import Navbar from './components/pages/universal/Navbar'
 
 export default function App() {
   return (
     <AuthProvider>
+      <Navbar/>
       <Routes>
         <Route exact path='/' element={<Homepage/>}/>
       </Routes>
