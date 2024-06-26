efc4f3b6007e22404ecf6c245eec9d304e7c06f7
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-26 03:54:25 +0300

    static fixed navbar added

--- frontend/src/components/pages/universal/Navbar.js
+++ frontend/src/components/pages/universal/Navbar.js
@@ -1,39 +1,39 @@
 function Navbar() {
     return ( 
         <div>
-            <nav style={{color:"white"}} class="navbar p-3 navbar-expand-md bg-secondary text-dark">
-                <div class="container">
-                    <a class="navbar-brand" href="#">APIs Development</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav style={{color:"white"}} className="navbar fixed-top p-4 navbar-expand-md bg-secondary text-dark">
+                <div className="container">
+                    <a className="navbar-brand" href="#">APIs Development</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Posts</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Posts</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">User</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Authentication</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">User</a>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Authentication</a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
                             </li>
                         </ul>
 
                         <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Login</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Login</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Register</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Register</a>
                             </li>
                         </ul>
                     </div>
