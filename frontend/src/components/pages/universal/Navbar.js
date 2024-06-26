5ff698ed6e755a0203050db4a6745bd7e98f3092
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-26 04:44:37 +0300

    logo added with alt

--- frontend/src/components/pages/universal/Navbar.js
+++ frontend/src/components/pages/universal/Navbar.js
@@ -1,39 +1,44 @@
+import { Link } from "react-router-dom";
+import Logo from '../../../images/logo.jpg'
+
 function Navbar() {
     return ( 
         <div>
             <nav style={{color:"white"}} className="navbar fixed-top p-4 navbar-expand-md bg-secondary text-dark">
                 <div className="container">
-                    <a className="navbar-brand" href="#">APIs Development</a>
+                    <Link className="navbar-brand" to="/">
+                        <img className="image-logo" src={Logo} alt="APIs Development"/>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarText">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link text-white active Link" aria-current="page" to="#">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Posts</a>
+                                <Link className="nav-link text-white Link" to="/posts">Posts</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">User</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Authentication</a>
+                                <Link className="nav-link text-white Link" to="/user">User</Link>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
+                                <Link className="nav-link text-white Link" to="/auth">Authentication</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link text-white" href="#">About</a>
                             </li>
                         </ul>
 
                         <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Login</a>
+                                <Link className="nav-link text-white" to="/api/auth/login">Login</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Register</a>
+                                <Link className="nav-link text-white" to="/api/auth/register">Register</Link>
                             </li>
                         </ul>
                     </div>
