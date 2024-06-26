11b573445e076590d0a54a5da3e11e76f8cd9e0a
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-26 03:53:01 +0300

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,21 @@ import { AuthProvider } from './components/utils/AuthProvider'
 // components
 import Homepage from './components/pages/universal/Homepage'
 import Navbar from './components/pages/universal/Navbar'
+import Register from './components/pages/auth/Register'
+import Login from './components/pages/auth/Login'
 
 export default function App() {
   return (
     <AuthProvider>
-      <Navbar/>
-      <Routes>
-        <Route exact path='/' element={<Homepage/>}/>
-      </Routes>
-    </AuthProvider>
+        <Navbar/>
+        <div className='container p-5 my-5'>
+          <Routes>
+            <Route exact path='/' element={<Homepage/>}/>
+            <Route  path='/apis/auth/register' element={<Register/>}/>
+            <Route  path='/apis/auth/login' element={<Login/>}/>
+          </Routes>
+        </div>
+      </AuthProvider>
+
   )
 }
