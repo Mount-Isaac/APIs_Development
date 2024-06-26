ddbb07875fdece8ebd5fc96a2c3d2492ef323aa0
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-26 03:53:38 +0300

    user methods

--- frontend/src/components/pages/Functions/UserDefinedFunctions.js
+++ frontend/src/components/pages/Functions/UserDefinedFunctions.js
@@ -1,9 +1,33 @@
-const UserDefinedFunctions = () => {
-    return ( 
-        <div>
-            UserDefinedFunctions
-        </div> 
-    );
-}
+// const UserDefinedFunctions = () => {
+//     return ( 
+//         <div>
+//             UserDefinedFunctions
+//         </div> 
+//     );
+// }
  
-export default UserDefinedFunctions;
+// export default UserDefinedFunctions;
+import { useContext } from "react"
+import AuthContext from "../../utils/AuthProvider"
+
+
+const HandleSubmit = (e) => {
+    e.preventDefault()
+    
+    const {formData} = useContext(AuthContext)
+
+    console.log(formData)
+    const endpoint = ''
+}
+
+
+const HandleChange =  (e) => {
+    const {setFormData} = useContext(AuthContext)
+    const {name, value} = e.target
+
+    setFormData(prevData => ({
+        ...prevData, 
+        [name]:value
+    }))
+}
+export {HandleSubmit, HandleChange}
