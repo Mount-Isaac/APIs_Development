0574aa3417999a1d04b1ad32e9df86bb4d24349f
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-26 04:43:33 +0300

    page404 component added

---
+++ frontend/src/components/pages/universal/Page404.js
@@ -0,0 +1,9 @@
+function Page404() {
+    return ( 
+        <div className="container p-5">
+            <h2 className="text-danger h1 text-center">Page 404!</h2>
+        </div>
+     );
+}
+
+export default Page404;
