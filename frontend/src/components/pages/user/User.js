f8bb7b5bc68f94590ddb2794073ddb2e38dd03a1
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-28 13:27:24 +0300

    register, login, update pass & profile endpoints updated

--- frontend/src/components/pages/user/User.js
+++ frontend/src/components/pages/user/User.js
@@ -1,153 +1,255 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import React, { useContext, useEffect, useState } from 'react';
 import Data from './Data'
 import AuthContext from '../../utils/AuthProvider';
 import axios from 'axios';
 
-const JsonEditor = ({object, auth, file}) => {
-    // console.log(file, 'file passed')
-    const [inputObject, setInputObject] = useState(JSON.stringify(object, null, 2));
-    const [formData, setFormData]  = useState()
-    const [oneRun, setOneRun] = useState(false)
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [fileAdded, setFileAdded] = useState(false)
-    const {user} = useContext(AuthContext)
-
-    const handleChange = (event) => {
-        setInputObject(event.target.value)
-    };
-
-    const handleBlur = () => {
-        try {
-            const parsedObject =  JSON.parse(inputObject);
-            setInputObject(JSON.stringify(parsedObject, null, 2))
-        } catch (error) {
-            console.log('Do not edit the  JSON object format')
-        }
-
-    }
-
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        setSelectedFile(file)
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setFormData(JSON.parse(inputObject))
-    
-        // Further processing can be added here, e.g., sending data to a server
-    };
-
-    useEffect(()=> {
-        if(formData && !oneRun){
-            setFormData(prevData =>({
-                ...prevData,
-                image : selectedFile
-            }))
-            setFileAdded(true)
-            setOneRun(true)
-        }
-
-    }, [formData])
-
-    useEffect(()=> {
-        if(fileAdded){
-            console.log(formData)
-            const my_data = new FormData()
-
-            for(let key in formData){
-                my_data.append(key, formData[key])
-            }
-        
-            setFileAdded(false)
-            // console.log(formData)
-
-            // send the data to backend and return response 
-            const registerUser = async (my_data) => {
-                try {
-                    const endpoint = 'http://localhost:8000/api/auth/register'
-                    const headers = {'Content-Type':'multipart/form-data'}
-    
-                    const {data, status} = await axios.post(endpoint, my_data, {headers})
-                    console.log(data, status)
-                    
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-            console.log(my_data)
-            registerUser(my_data)
-
-        }
-
-    }, [fileAdded,formData])
-
-    return (
-        <div>
-            {
-                auth ?
-                user.email ?
-                    <>
-                        <h5 className='text-center'>Edit user object and upload image</h5>
-                        <form onSubmit={handleSubmit}>
-                            <textarea
-                                id='textarea'
-                                rows= {file ? "8" : "5"}
-                                cols="50"
-                                value={inputObject}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                            <br />
-                            {
-                                file && 
-                                <>
-                                    <label htmlFor="fileInput">Upload Image:</label><br />
-                                    <input required className='input-group mb-2' type="file" id="fileInput" name="fileInput" onChange={handleFileChange}/>
-                                </>
-                            }
-                            <input type="submit" value="Submit" />
-                        </form>
-                    </>
-                :
-                <div>
-                    You must be authenticated to access this route
-                </div>
-                :
-                <>
-                        <h5 className='text-center'>Edit user object and upload image</h5>
-                        <form onSubmit={handleSubmit}>
-                            <textarea
-                                id='textarea'
-                                rows= {file ? "8" : "5"}
-                                cols="50"
-                                value={inputObject}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                            <br />
-
-                            {
-                                file && 
-                                <>
-                                    <label htmlFor="fileInput">Upload Image:</label><br />
-                                    <input required className='input-group mb-2' type="file" id="fileInput" name="fileInput" onChange={handleFileChange}/>
-                                </>
-                            }
-                            <input type="submit" value="Submit" />
-                        </form>
-                    </>
-            }
-        </div>
-    );
-};
 
 function User() {
     const arrowElement = <FontAwesomeIcon icon={faChevronDown} size='lg'/>
     const [show, setShow] = useState(new Array(Data.length).fill(false))
     const [test, setTest] = useState(new Array(Data.length).fill(false))
+    const [userCreated, setUserCreated] = useState(new Array(Data.length).fill(false))
+    const [returnedData, setReturnedData] = useState(new Array(Data.length).fill({}))
+
+    const {user} = useContext(AuthContext)
+
+    const handleUserCreated = (index) => {
+        setUserCreated(prevState => {
+            const newState = [...prevState]
+            newState[index] = !newState[index]
+            return newState
+        })
+    }
+
+    const handleSetReturnedData = (index, data) => {
+        console.log('before', userCreated[index])
+        // handleUserCreated(index)
+
+        setUserCreated(prevState => {
+            const newState = [...prevState]
+            newState[index] = !newState[index]
+            return newState
+        })
+
+        setReturnedData(prevState => {
+            const newState = [...prevState]
+            newState[index] = data
+            return newState
+        })
+        // handle user created
+        console.log('after', userCreated[index])
+
+    }
+
+    // console.log(returnedData)
+    // console.log(userCreated)
+    const JsonEditor = ({obj, index}) => {
+        // console.log(file, 'file passed')
+        const [inputObject, setInputObject] = useState(JSON.stringify(obj.object, null, 2));
+        const [formData, setFormData]  = useState()
+        const [oneRun, setOneRun] = useState(false)
+        const [selectedFile, setSelectedFile] = useState(null);
+        const [fileAdded, setFileAdded] = useState(false)
+        const {user, setUser, setAuthTokens, authTokens} = useContext(AuthContext)
+        const [loading, setLoading] = useState(false)
+        const spinner = <FontAwesomeIcon icon={faSpinner} spin />
+    
+        const handleChange = (event) => {
+            setInputObject(event.target.value)
+        };
+    
+        const handleBlur = () => {
+            try {
+                const parsedObject =  JSON.parse(inputObject);
+                setInputObject(JSON.stringify(parsedObject, null, 2))
+            } catch (error) {
+                console.log('Do not edit the  JSON object format')
+            }
+    
+        }
+    
+        const handleFileChange = (event) => {
+            const file = event.target.files[0];
+            setSelectedFile(file)
+        };
+    
+        const handleSubmit = (event) => {
+            event.preventDefault()
+            setFormData(JSON.parse(inputObject))
+        
+            // Further processing can be added here, e.g., sending data to a server
+        };
+    
+        useEffect(()=> {
+            if(formData && !oneRun){
+                setFormData(prevData =>({
+                    ...prevData,
+                    image : selectedFile
+                }))
+                setFileAdded(true)
+                setOneRun(true)
+            }
+    
+        }, [formData])
+    
+        useEffect(()=> {
+            if(fileAdded){
+                console.log(formData)
+                const my_data = new FormData()
+    
+                for(let key in formData){
+                    my_data.append(key, formData[key])
+                }
+            
+                setFileAdded(false)
+                // console.log(formData)
+    
+                // send the data to backend and return response 
+                const registerUser = async (my_data) => {
+                    let headers;
+                    try {
+                        setLoading(true)
+                        obj.auth ? 
+                            headers = {
+                                'Content-Type':'application/json',
+                                'Authorization': `Bearer ${authTokens.access}`
+                            }
+                            :
+                            headers = {'Content-Type':'multipart/form-data'}
+
+        
+                        const {data, status} = await axios.post(obj.url, my_data, {headers})
+                        console.log(status, data)
+                        if(status === obj.code){
+                            if(status === 202){
+                                // handleUserCreated(index)
+                                handleSetReturnedData(index, data)
+                                // console.log('password updated successfully.')
+                            }else if(status === 200){
+                                // user login
+                                handleSetReturnedData(index, data.user)
+                                setUser(data.user)
+                                setAuthTokens({
+                                    access: data.access,
+                                    refresh: data.refresh
+                                })
+                                // cache the user & tokens
+                                localStorage.setItem('Tokens', JSON.stringify(
+                                    {
+                                        "refresh": data.refresh,
+                                        "access": data.access 
+                                    }
+                                ))
+                                localStorage.setItem('customer', JSON.stringify(data.user))
+                
+                                setUser(data.user)
+                                setAuthTokens({
+                                    "refresh": data.refresh,
+                                    "access": data.access 
+                                })
+                                // handleUserCreated(index)
+
+                            }else if(status === 204){
+                                // user deleted
+                                handleSetReturnedData(index, {'data':'You account has been deleted successfully.'})
+                                setUser({user:''})
+                                setAuthTokens('')
+                                // handleUserCreated(index)
+                            }
+                            // handle endpoint data posting
+                            // handleUserCreated(index)
+                            setLoading(false)
+                        }else{
+                            // handleUserCreated(index)
+                            handleSetReturnedData(index, {"Error": "Refresh the page and try again."})
+                            setLoading(false)
+                        }
+                        // console.log(data, status)
+                        
+                    } catch (error) {
+                        // handleUserCreated(index)
+                        setLoading(false)
+                        handleSetReturnedData(index, {"Error": "Refresh the page and try again."})
+                        console.log(error)
+                    }
+                }
+                console.log(my_data)
+                registerUser(my_data)
+    
+            }
+    
+        }, [fileAdded,formData])
+    
+        return (
+            <div>
+                {
+                    obj.auth ?
+                    user.email ?
+                        <>
+                            <h5 className='text-center'>Edit user object and upload image</h5>
+                            <form onSubmit={handleSubmit}>
+                                <textarea
+                                    id='textarea'
+                                    rows= {obj.file ? "8" : "5"}
+                                    cols="50"
+                                    value={inputObject}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                <br />
+                                {
+                                    obj.file && 
+                                    <>
+                                        <label htmlFor="fileInput">Upload Image:</label><br />
+                                        <input required className='input-group mb-2' type="file" id="fileInput" name="fileInput" onChange={handleFileChange}/>
+                                    </>
+                                }
+                                <div className='text-center'>
+                                    <button style={{width:150}}>
+                                        {loading ? spinner : "Submit"}
+                                    </button>
+                                </div>
+                            </form>
+                        </>
+                    :
+                    <div>
+                        You must be authenticated to access this route
+                    </div>
+                    :
+                    <>
+                            <h5 className='text-center'>Edit user object and upload image</h5>
+                            <form onSubmit={handleSubmit}>
+                                <textarea
+                                    id='textarea'
+                                    rows= {obj.file ? "8" : "5"}
+                                    cols="50"
+                                    value={inputObject}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                <br />
+    
+                                {
+                                    obj.file && 
+                                    <>
+                                        <label htmlFor="fileInput">Upload Image:</label><br />
+                                        <input required className='input-group mb-2' type="file" id="fileInput" name="fileInput" onChange={handleFileChange}/>
+                                    </>
+                                }
+                                <div className='text-center'>
+                                    <button style={{width:150}}>
+                                        {loading ? spinner : "Submit"}
+                                    </button>
+
+                                </div>
+                            </form>
+                        </>
+                }
+            </div>
+        );
+    };
     
     const handleShow = (index) => {
         // console.log(index)
@@ -166,6 +268,8 @@ function User() {
             return newState
         })
     }
+
+
     return ( 
         <div className="container p-2 users-container">
             <h3 className="text-center">User endpoints</h3> 
@@ -196,26 +300,30 @@ function User() {
                                 </p>
 
                                 <div className='px-1 row row-testing'>
-                                    {
+                                    {userCreated[index] ? (
+                                        <pre>
+                                            {
+                                                test[index] &&
+                                                JSON.stringify(returnedData[index], null, 2)
+                                            }
+                                        </pre>
+                                    ) : (
+                                        <>
+                                            {!obj.auth && (
+                                                <div className='col-lg-6 col-md-6 col-sm-12 bg-dark text-white'>
+                                                    <pre>{JSON.stringify(obj.object, null, 4)}</pre>
+                                                </div>
+                                            )}
 
-                                        !obj.auth &&
-                                        <div className='col-lg-6 col-md-6 col-sm-12 bg-dark text-white'>
-                                            <pre>
-                                                {
-                                                    JSON.stringify(obj.object, null, 4)
-                                                }
-                                            </pre>
-                                        </div>
-                                    }
-
-                                    {
-                                        test[index] &&      
-                                        <div className='col-lg-6 col-md-6 col-sm-12'>
-                                            <JsonEditor object={obj.object} auth={obj.auth} file={obj.file}/>                                        
-                                        </div>
-                                    }
-
+                                            {test[index] && (
+                                                <div className='col-lg-6 col-md-6 col-sm-12'>
+                                                    <JsonEditor obj={obj} index={index} />
+                                                </div>
+                                            )}
+                                        </>
+                                    )}
                                 </div>
+
                             </div>
                         }
                         </p>
