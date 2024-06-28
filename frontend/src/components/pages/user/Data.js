68fdbdfae3e648d40fbffddbcfb56c40718eade3
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-28 13:26:35 +0300

    APIs endpoints data objects updated

--- frontend/src/components/pages/user/Data.js
+++ frontend/src/components/pages/user/Data.js
@@ -1,9 +1,11 @@
 const Data = [
     {
         auth:false,
+        url:"http://localhost:8000/api/auth/register",
+        endpoint:"/api/auth/register",
+        code:201,
         file:true,
         title: "Create user",
-        endpoint: "/api/auth/register",
         text: "Create a user account by posting profile data to this endpoint",
         object: {
             email: '',
@@ -13,9 +15,13 @@ const Data = [
             password: '',
         }
     },
+
+
     {
         auth:false,
+        url:"http://localhost:8000/api/auth/login",
         file:false,
+        code:200,
         title: "Login",
         endpoint: "/api/auth/login",
         text: "Authenticate and log in a user by posting credentials to this endpoint",
@@ -25,24 +31,30 @@ const Data = [
         }
     },
 
+
     {
         auth:true,
+        url:"http://localhost:8000/api/user/update/password",
+        endpoint:"/api/user/update/password",
         title: "Change Password",
-        endpoint: "/api/auth/change-password",
+        code:202,
         text: "Change the user's password by posting new password data to this endpoint",
         object: {
             old_password: '',
             new_password: '',
         }
     },
+
+
     {
         auth:true,
-        file:true,
+        url:"http://localhost:8000/api/user/update/profile",
+        endpoint:"/api/user/update/profile",
+        code:202,
+        file:false,
         title: "Update User",
-        endpoint: "/api/users/update",
         text: "Update user profile information by posting updated data to this endpoint",
         object: {
-            email: '',
             first_name: '',
             last_name: '',
             phone_number: '',
@@ -51,12 +63,20 @@ const Data = [
 
     {
         auth:true,
+        url:"http://localhost:8000/api/user/delete/",
+        endpoint:"/api/user/delete/",
+        code:204,
         title: "Delete User",
         endpoint: "/api/users/delete",
         text: "Delete a user account by sending a request to this endpoint",
     },
+
+
     {
         auth:true,
+        url:"http://localhost:8000/api/user/update/email",
+        endpoint:"/api/user/update/email",
+        code:200,
         title: "Change Email",
         endpoint: "/api/users/change-email",
         text: "Change the user's email address by posting new email data to this endpoint",
