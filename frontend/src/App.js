6723f04004c250143d7ba90d0d8a4f8ee9703947
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-06-28 15:52:32 +0300

    new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,19 @@ import PrivateRoutes from './components/utils/PrivateRoutes'
 // components
 import Homepage from './components/pages/universal/Homepage'
 import Navbar from './components/pages/universal/Navbar'
+import Page404 from './components/pages/universal/Page404'
+import About from './components/pages/universal/About'
+
 import Register from './components/pages/auth/Register'
 import Login from './components/pages/auth/Login'
-import Page404 from './components/pages/universal/Page404'
-import Post from './components/pages/posts/Posts'
 import User from './components/pages/user/User'
-import About from './components/pages/universal/About'
+
+import CreatePost from './components/pages/posts/CreatePost'
+import ViewPost from './components/pages/posts/ViewPost'
+import EditPost from './components/pages/posts/EditPost'
+import AllPosts from './components/pages/posts/AllPosts'
+import UserPosts from './components/pages/posts/UserPosts'
+import Post from './components/pages/posts/Post'
 
 export default function App() {
   return (
@@ -19,13 +26,25 @@ export default function App() {
         <Navbar/>
         <div className='container p-5 my-5'>
           <Routes>
+            {/* requires authentications */}
             <Route element={<PrivateRoutes/>}>
-              <Route path='/api/post' element={<Post/>}/>
+              <Route path='/api/post/create' element={<CreatePost/>}/>
+              <Route path='/api/post/view/:id' element={<ViewPost/>}/>
+              <Route path='/api/post/edit/:id' element={<EditPost/>}/>
             </Route>
-              <Route path='/api/user' element={<User/>}/>
-            <Route exact path='/' element={<Homepage/>}/>
+
+            {/* user routes */}
+            <Route path='/api/user' element={<User/>}/>
             <Route  path='/api/auth/register' element={<Register/>}/>
             <Route  path='/api/auth/login' element={<Login/>}/>
+
+            {/* posts routes */}
+            <Route path='/api/post' element={<Post/>}/>
+            <Route path='/api/posts/all/' element={<AllPosts/>}/>
+            <Route path='/api/posts/user/:id' element={<UserPosts/>}/>
+
+            {/* universal routes */}
+            <Route exact path='/' element={<Homepage/>}/>
             <Route path='/api/about' element={<About/>}/>
             <Route  path='*' element={<Page404/>}/>
           </Routes>
