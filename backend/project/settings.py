928b58639937e6d7bb5feff61a0e6490131489a2
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-07-01 19:14:53 +0300

    settings changed

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -170,11 +170,11 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": False,
-    "UPDATE_LAST_LOGIN": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": True,
 }
 
 CLOUDINARY_STORAGE = {
