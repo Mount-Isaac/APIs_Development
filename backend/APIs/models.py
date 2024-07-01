8505cbe0bd837be8e654bebb77872935e88ed4ed
Isaac Kyalo <kyalomutie@students.ku.ac.ke>
2024-07-01 19:13:46 +0300

    Comment & Post models altered

--- backend/APIs/models.py
+++ backend/APIs/models.py
@@ -58,8 +58,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=50)
     content = models.TextField(blank=False, null=False)
+    liked = models.BooleanField(default=False)
+    disliked = models.BooleanField(default=False)
     likes = models.ManyToManyField(User, related_name='liked_by', blank=True)
     dislikes = models.ManyToManyField(User, related_name='disliked_by', blank=True)
+    comment = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     # add cloudinary image upload
@@ -90,6 +93,10 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField()
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(blank=False, null=False, default='')
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.post.title} {self.user.email}'
